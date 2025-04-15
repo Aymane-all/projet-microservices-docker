9f227730044b130bce0f4f39996428fcfb31f3e1
Aymane Abbad <aymaneel560@gmail.com>
2025-04-15 23:40:15 +0100

    ..

--- node-service/index.js
+++ node-service/index.js
@@ -1,18 +1,18 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const app = express();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
 
-// app.use(cors());
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World');
-// })
-// app.get('/hello', (req, res) => {
-//     res.send('Hello hi');
-// })
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+app.get('/hello', (req, res) => {
+    res.send('Hello hi');
+})
 
-// app.listen(5000,()=>{
-//     console.log('Rest api server listening on port 5000');
-// })
+app.listen(5000,()=>{
+    console.log('Rest api server listening on port 5000');
+})
